f0cec9ecfa00bb86a49f58d434d0fa0844a395bb
Durgesh kumar <kdurgesh029@gmail.com>
2020-10-10 07:04:57 +0530

    Update database.php

--- database.php
+++ database.php
@@ -6,13 +6,13 @@ public class database ()
 $pass="MyPass";
 $uname="MyUname";
 
-connect($dbname)
+function connect($dbname)
         {
 
 $this.$conn=mysqli_connect($uname,$pass,$dbname) or die("Could Not connect") ;
 return $this.$conn;
 }
-close()
+function close()
 {
 
 mysqli_close($this.$conn);
